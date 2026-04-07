78aa51d97e741d1c74dadc3bbd440c3d672cc5ec
SinanErbezci <sinanerbezci@hotmail.com>
2026-04-07 21:47:44 +0300

    serializer correction

--- bookclub/library/serializer.py
+++ bookclub/library/serializer.py
@@ -1,16 +1,18 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, Author
 
 
 class BookSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
+    author_name = serializers.CharField(source="author.name", read_only=True )
 
     class Meta:
         model = Book
-        fields = [
-            "id",
-            "title",
-            "author",
-            "rating",
-            "num_ratings",
-        ]
+        fields = ["id", "title", "rating", "cover", "author", "author_name"]
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+    book_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ["id", "name", "books", "book_count"]
