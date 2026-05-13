92b4a4211015944def85908f8896a69aa697e73b
SinanErbezci <sinanerbezci@hotmail.com>
2026-05-13 13:00:01 +0300

    added new settings

--- bookclub/mysite/settings/production.py
+++ bookclub/mysite/settings/production.py
@@ -16,6 +16,9 @@ CSRF_TRUSTED_ORIGINS = [
     "https://bookclub-peach-six.vercel.app",
 ]
 
+SESSION_COOKIE_SAMESITE = "None"
+CSRF_COOKIE_SAMESITE = "None"
+
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
