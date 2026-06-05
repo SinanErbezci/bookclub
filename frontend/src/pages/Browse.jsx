5e40031bf840490c3f34adfbf05879b447a8b3df
sinan_linux <sinanerbezci@hotmail.com>
2026-06-05 10:47:34 +0300

    quick fix

--- frontend/src/pages/Browse.jsx
+++ frontend/src/pages/Browse.jsx
@@ -5,7 +5,6 @@ import FeaturedGenre from "../features/FeaturedGenre";
 function Browse() {
   return (
     <div className="container mt-4">
-      <h1>deneme</h1>
       <RecentlyAdded />
       <FeaturedAuthor />
       <FeaturedGenre />
