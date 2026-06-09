0690b3da83d1682e100a39c43d46be14c2dfcc13
sinan_linux <sinanerbezci@hotmail.com>
2026-06-09 17:20:50 +0300

    Import IAM and security groups into Terraform

---
+++ infra/networking.tf
@@ -0,0 +1,86 @@
+resource "aws_security_group" "ec2" {
+    name = "launch-wizard-6"
+    description = "launch-wizard-6 created 2026-06-04T08:46:00.889Z"
+
+    vpc_id = "vpc-02871713f3675bb3e"
+
+    tags = local.common_tags
+}
+
+resource "aws_security_group" "alb" {
+  name        = "alb-security-group"
+  description = "bookclub"
+
+  vpc_id = "vpc-02871713f3675bb3e"
+
+  tags = local.common_tags
+}
+
+resource "aws_vpc_security_group_egress_rule" "ec2_all_outbound" {
+
+  security_group_id = aws_security_group.ec2.id
+
+  ip_protocol = "-1"
+
+  cidr_ipv4 = "0.0.0.0/0"
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http_from_alb" {
+
+  security_group_id = aws_security_group.ec2.id
+
+  ip_protocol = "tcp"
+
+  from_port = 80
+  to_port   = 80
+
+  referenced_security_group_id = aws_security_group.alb.id
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+
+  security_group_id = aws_security_group.ec2.id
+
+  ip_protocol = "tcp"
+
+  from_port = 22
+  to_port   = 22
+
+  cidr_ipv4 = "94.55.25.219/32"
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_http" {
+
+  security_group_id = aws_security_group.alb.id
+
+  ip_protocol = "tcp"
+
+  from_port = 80
+  to_port   = 80
+
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_https" {
+
+  security_group_id = aws_security_group.alb.id
+
+  ip_protocol = "tcp"
+
+  from_port = 443
+  to_port   = 443
+
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_all_outbound" {
+
+  security_group_id = aws_security_group.alb.id
+
+  ip_protocol = "-1"
+
+  cidr_ipv4 = "0.0.0.0/0"
+}
