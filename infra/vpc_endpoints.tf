5347372cd7a3fc611037b93bf9ecb68feeff0344
sinan_linux <sinanerbezci@hotmail.com>
2026-07-03 17:12:59 +0300

    deleted vpc endpoints

--- infra/vpc_endpoints.tf
+++ infra/vpc_endpoints.tf
@@ -25,100 +25,100 @@ resource "aws_vpc_security_group_egress_rule" "vpc_endpoints_all" {
   cidr_ipv4 = "0.0.0.0/0"
 }
 
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.ssm"
-  vpc_endpoint_type = "Interface"
+# resource "aws_vpc_endpoint" "ssm" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.${var.aws_region}.ssm"
+#   vpc_endpoint_type = "Interface"
 
-  subnet_ids = [
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id
-  ]
+#   subnet_ids = [
+#     aws_subnet.private_a.id,
+#     aws_subnet.private_b.id
+#   ]
 
-  security_group_ids = [
-    aws_security_group.vpc_endpoints.id
-  ]
+#   security_group_ids = [
+#     aws_security_group.vpc_endpoints.id
+#   ]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.ec2messages"
-  vpc_endpoint_type = "Interface"
+# resource "aws_vpc_endpoint" "ec2messages" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.${var.aws_region}.ec2messages"
+#   vpc_endpoint_type = "Interface"
 
-  subnet_ids = [
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id
-  ]
+#   subnet_ids = [
+#     aws_subnet.private_a.id,
+#     aws_subnet.private_b.id
+#   ]
 
-  security_group_ids = [
-    aws_security_group.vpc_endpoints.id
-  ]
+#   security_group_ids = [
+#     aws_security_group.vpc_endpoints.id
+#   ]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.ssmmessages"
-  vpc_endpoint_type = "Interface"
+# resource "aws_vpc_endpoint" "ssmmessages" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.${var.aws_region}.ssmmessages"
+#   vpc_endpoint_type = "Interface"
 
-  subnet_ids = [
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id
-  ]
+#   subnet_ids = [
+#     aws_subnet.private_a.id,
+#     aws_subnet.private_b.id
+#   ]
 
-  security_group_ids = [
-    aws_security_group.vpc_endpoints.id
-  ]
+#   security_group_ids = [
+#     aws_security_group.vpc_endpoints.id
+#   ]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.ecr.api"
-  vpc_endpoint_type = "Interface"
+# resource "aws_vpc_endpoint" "ecr_api" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.${var.aws_region}.ecr.api"
+#   vpc_endpoint_type = "Interface"
 
-  subnet_ids = [
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id
-  ]
+#   subnet_ids = [
+#     aws_subnet.private_a.id,
+#     aws_subnet.private_b.id
+#   ]
 
-  security_group_ids = [
-    aws_security_group.vpc_endpoints.id
-  ]
+#   security_group_ids = [
+#     aws_security_group.vpc_endpoints.id
+#   ]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id            = aws_vpc.main.id
-  service_name      = "com.amazonaws.${var.aws_region}.ecr.dkr"
-  vpc_endpoint_type = "Interface"
+# resource "aws_vpc_endpoint" "ecr_dkr" {
+#   vpc_id            = aws_vpc.main.id
+#   service_name      = "com.amazonaws.${var.aws_region}.ecr.dkr"
+#   vpc_endpoint_type = "Interface"
 
-  subnet_ids = [
-    aws_subnet.private_a.id,
-    aws_subnet.private_b.id
-  ]
+#   subnet_ids = [
+#     aws_subnet.private_a.id,
+#     aws_subnet.private_b.id
+#   ]
 
-  security_group_ids = [
-    aws_security_group.vpc_endpoints.id
-  ]
+#   security_group_ids = [
+#     aws_security_group.vpc_endpoints.id
+#   ]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
