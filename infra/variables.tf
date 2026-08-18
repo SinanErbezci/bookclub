3ddb89cced6ee695a169e9b8e20af9fc5697d2ba
SinanErbezci <sinanerbezci@hotmail.com>
2026-08-18 12:47:22 +0300

    cloudflare

--- infra/variables.tf
+++ infra/variables.tf
@@ -24,3 +24,15 @@ variable "backend_image_tag" {
   type        = string
   default     = "latest"
 }
+
+variable "cloudflare_api_token" {
+  description = "Cloudflare API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare zone ID for sinanerbezci.com"
+  type        = string
+}
+
