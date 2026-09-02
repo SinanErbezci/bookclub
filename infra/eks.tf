c8431f0ab6d3666abf11a257b185193618f5d030
SinanErbezci <sinanerbezci@hotmail.com>
2026-09-02 14:17:20 +0300

    remove old requirements files and add EKS node group configuration

--- infra/eks.tf
+++ infra/eks.tf
@@ -16,3 +16,37 @@ resource "aws_eks_cluster" "bookclub" {
     aws_iam_role_policy_attachment.eks_cluster_policy
   ]
 }
+
+resource "aws_eks_node_group" "bookclub" {
+  count = var.production_enabled ? 1 : 0
+
+  cluster_name    = aws_eks_cluster.bookclub[0].name
+  node_group_name = "${var.project_name}-nodes"
+  node_role_arn   = aws_iam_role.eks_node[0].arn
+
+  subnet_ids = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id,
+  ]
+
+  instance_types = ["t3.medium"]
+  capacity_type  = "ON_DEMAND"
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 1
+    max_size     = 2
+  }
+
+  disk_size = 20
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node,
+    aws_iam_role_policy_attachment.eks_cni,
+    aws_iam_role_policy_attachment.eks_ecr
+  ]
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-eks-node"
+  })
+}
